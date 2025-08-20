b8b63a2eafd1066decf39e60179511e3495ea719
Matibus-net <matibuscemi123@gmail.com>
2025-08-20 16:10:11 -0300

    20/08/2025

--- Java/src/Parciales/Parcial6/parcial6.java
+++ Java/src/Parciales/Parcial6/parcial6.java
@@ -16,8 +16,8 @@ public class parcial6 {
         paciente p2 = new paciente("juan",200,true);
         paciente p3 = new paciente("ser",200,true);
         paciente p4 = new paciente("sil",200,true);
-        paciente p5 = new paciente("nati",200,true);
-        paciente p6 = new paciente("ine",200,true);
+        paciente p5 = new paciente("irma",200,true);
+        paciente p6 = new paciente("javier",200,true);
         a.agendar(p6, 1, 2);
         a.agendar(p1, 2, 2);
         a.agendar(p2, 3, 2);
@@ -26,15 +26,15 @@ public class parcial6 {
         a.agendar(p5, 1, 1);
         a.agendar(p6, 4, 2);
         a.agendar(p6, 1, 5);
-        a.liberarTurnos("ine");
-       System.out.println( a.tieneTurno("ine", 1));
-       System.out.println( a.tieneTurno("nati", 1));
+        a.liberarTurnos("irma");
+       System.out.println( a.tieneTurno("irma", 1));
+       System.out.println( a.tieneTurno("javier", 1));
        System.out.println(a.agendarPrimerdia(p6, 3));
        System.out.println(a.agendarPrimerdia(p6, 5));
-       System.out.println( a.tieneTurno("ine", 1));
-       System.out.println(a.cantidadTurnos("ine"));
-       a.liberarDia("ine",1);
-       System.out.println(a.cantidadTurnos("ine"));
+       System.out.println( a.tieneTurno("irma", 1));
+       System.out.println(a.cantidadTurnos("irma"));
+       a.liberarDia("irma",1);
+       System.out.println(a.cantidadTurnos("irma"));
        System.out.println(a.cantidadTurnos("mati"));
        
     }        
